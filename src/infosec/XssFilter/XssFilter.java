bc76fc01fa771885094ce5cdb2ad719b2fe24c12
zhangzonghan1338 <zhangzonghan1338@Oscar>
2016-06-04 10:44:34 +0800

    aaa

---
+++ src/infosec/XssFilter/XssFilter.java
@@ -0,0 +1,30 @@
+package infosec.XssFilter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import infosec.XssHttpWrapper.XssHttpWrapper;
+
+public class XssFilter implements Filter {
+	 
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        System.out.println("Xss filter inited!");
+    }
+ 
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        XssHttpWrapper xssRequest = new XssHttpWrapper((HttpServletRequest) request);
+        chain.doFilter(xssRequest, response);
+    }
+ 
+    @Override
+    public void destroy() {
+        System.out.println("Xss filter destroyed!");
+    }
+}
